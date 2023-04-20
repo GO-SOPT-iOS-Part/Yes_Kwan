f7851a6f61d8a9f98a0955d12f458de3d7fa04ec
김응관 <kwhn9486@naver.com>
2023-04-20 16:05:48 +0900

    [Add] TVING 로고 이미치 Asset에 추가 #4

--- Sopt_Seminar/Sopt_Seminar/Week2/Assignment/LoginSuccessViewController.swift
+++ Sopt_Seminar/Sopt_Seminar/Week2/Assignment/LoginSuccessViewController.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginSuccessViewController: UIViewController {
+    
+    // 1. TVING image
+    private let logoImg: UIImageView = {
+        let logo = UIImageView()
+        logo.image = UIImage()
+    }()
+}
