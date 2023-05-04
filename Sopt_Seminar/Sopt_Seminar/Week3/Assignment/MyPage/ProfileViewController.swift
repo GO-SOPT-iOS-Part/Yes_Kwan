96534cc16b894ee4a8eb5fc183a7abbb06d95b31
김응관 <kwhn9486@naver.com>
2023-05-04 21:04:32 +0900

    [Add] 프로필사진 Asset에 추가 #7

---
+++ Sopt_Seminar/Sopt_Seminar/Week3/Assignment/MyPage/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Sopt_Seminar
+//
+//  Created by 김응관 on 2023/05/04.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
