76bd43b35cb9dbd2aea72756b8fc0e67d8b30c77
김응관 <kwhn9486@naver.com>
2023-05-05 01:02:38 +0900

    [Move] 프로필 정보뷰 코드 별도의 파일로 분리 #7

--- Sopt_Seminar/Sopt_Seminar/Week3/Assignment/MyPage/MyPageViewController.swift
+++ Sopt_Seminar/Sopt_Seminar/Week3/Assignment/MyPage/MyPageViewController.swift
@@ -10,40 +10,29 @@ import SnapKit
 
 class MyPageViewController: UIViewController {
     
-    // 1. profile image
-    private let profile: UIImageView = {
-        let profile = UIImageView()
-        profile.image = UIImage(named: "profile")
-        profile.layer.cornerRadius = 15
-        profile.clipsToBounds = true
-        return profile
+    // 스크롤 뷰 생성
+    private let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
     }()
     
-    // 2. name label
-    private let name: UILabel = {
-        let name = UILabel()
-        name.text = "kwhn"
-        name.textColor = .white
-        name.font = .boldSystemFont(ofSize: 18.0)
-        return name
-    }()
-    
-    // 3. 프로필 전환 버튼
-    private lazy var button: UIButton = {
-        let btn = UIButton()
-        btn.setTitle("프로필 전환", for: .normal)
-        btn.setTitleColor(.white, for: .normal)
-        btn.titleLabel?.font = .systemFont(ofSize: 15)
-        btn.layer.borderColor = UIColor(named: "darkest")?.cgColor
-        btn.layer.cornerRadius = 5
-        btn.layer.borderWidth = 1.0
-        btn.backgroundColor = .black
-        return btn
+    // 0. 뷰들을 담을 Vertical StackView
+    private let stackView: UIStackView = {
+        let vertical = UIStackView()
+        vertical.translatesAutoresizingMaskIntoConstraints = false
+        vertical.axis = .vertical
+        vertical.spacing = 5
+        return vertical
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setStyle()
+        viewSetting()
+        
         setLayOut()
         
         // 1. bell button
@@ -56,43 +45,39 @@ class MyPageViewController: UIViewController {
         
         // 3. rightbarbutton에 추가
         self.navigationItem.rightBarButtonItems = [gear, bell]
+        
+        let profileView = ProfileView()
+        stackView.addArrangedSubview(profileView)
     }
 }
 
 extension MyPageViewController {
     
     func setStyle() {
-        view.backgroundColor = .black
+        view.backgroundColor = .white
+    }
+    
+    func viewSetting() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
     }
     
     func setLayOut() {
         
-        [profile, name, button].forEach {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview($0)
-        }
-        
+        // 3. 레이아웃 설정
         NSLayoutConstraint.activate([
-            profile.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30.0),
-            profile.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15.0)
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
         ])
-        
-        profile.snp.makeConstraints {
-            $0.height.equalTo(80)
-            $0.width.equalTo(80)
-        }
-        
-        name.snp.makeConstraints {
-            $0.leading.equalTo(profile.snp.trailing).offset(25)
-            $0.centerY.equalTo(profile.snp.centerY)
-        }
-        
-        button.snp.makeConstraints {
-            $0.centerY.equalTo(profile.snp.centerY)
-            $0.width.equalTo(100)
-            $0.height.equalTo(40)
-            $0.trailing.equalToSuperview().inset(15)
-        }
     }
     
     @objc
--- Sopt_Seminar/Sopt_Seminar/Week3/Assignment/MyPage/ProfileViewController.swift
+++
@@ -1,7 +0,0 @@
-//
-//  ProfileViewController.swift
-//  Sopt_Seminar
-//
-//  Created by 김응관 on 2023/05/04.
-//
-
