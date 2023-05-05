6b899574c4b83ae8bd8b2ac03d81a697d622808b
김응관 <kwhn9486@naver.com>
2023-05-05 12:12:09 +0900

    [Move] view 파일들 별도의 폴더에 정리 #7

---
+++ Sopt_Seminar/Sopt_Seminar/Week3/Assignment/MyPage/SettingTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingTableViewController.swift
+//  Sopt_Seminar
+//
+//  Created by 김응관 on 2023/05/05.
+//
+
+import UIKit
+
+class SettingTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
