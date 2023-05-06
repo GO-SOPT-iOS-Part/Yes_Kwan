5d88fc63aa5519036768668aa8493129bb06087e
김응관 <kwhn9486@naver.com>
2023-05-06 23:46:04 +0900

    WeatherAPI AlamoFire 실습 코드 추가 및 파일정리

---
+++ Sopt_Seminar/Sopt_Seminar/Week4/WeatherNetwork/WeatherViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  WeatherAPI_Practice
+//
+//  Created by 김응관 on 2023/05/06.
+//
+
+import UIKit
+import SnapKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getWeather()
+    }
+    
+    private func getWeather() {
+        
+        WeatherService.shared.getInfo(key: "0d268b100aab2067be1aeebe6b626058",
+                                    city: "seoul") { response in
+
+            switch response {
+            case .success(let data):
+                guard let data = data as? Weathers else { return }
+                dump(data)
+            default:
+                return
+            }
+        }
+    }
+}
+
