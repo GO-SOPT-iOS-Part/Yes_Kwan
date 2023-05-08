f87c9cfeabbcb96d70f59080c39b4edf6a3ae8d5
김응관 <kwhn9486@naver.com>
2023-05-09 03:39:35 +0900

    [Add] Custom SegmentedControl 추가 #11

--- Sopt_Seminar/Sopt_Seminar/Week3/Assignment/MainViewController/MainViewController.swift
+++ Sopt_Seminar/Sopt_Seminar/Week3/Assignment/MainViewController/MainViewController.swift
@@ -10,6 +10,13 @@ import SnapKit
 
 class MainViewController: UIViewController {
     
+    // SubViews
+    private let homeViewController = HomeViewController()
+    private let realTimeViewController = RealTimeViewController()
+    
+    // optional VC (default: mainViewController)
+    private lazy var targetVC: UIViewController = homeViewController
+    
     // 1. BarleftButton
     lazy private var leftButton: UIBarButtonItem = {
         let leftBtn = UIBarButtonItem(image: UIImage(named: "logo_tving")?.resized(toWidth: 99), style: .plain, target: self, action: #selector(scrollUp))
@@ -31,15 +38,25 @@ class MainViewController: UIViewController {
         return rightBtn
     }()
     
-        
+    // 3. Segmented Control
+    private let segmentedControl: UISegmentedControl = {
+        let control = SegmentControl(items: ["홈", "실시간", "TV", "영화", "파라마운트+"])
+        control.translatesAutoresizingMaskIntoConstraints = false
+        control.sizeToFit()
+        return control
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setStyle()
+        setLayOut()
+        setSegment()
         
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "logo_tving")?.resized(toWidth: 99), style: .plain, target: self, action: #selector(scrollUp))
-
-        // NavigationItem 색상 지정
         self.navigationItem.leftBarButtonItem = leftButton
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,11 +69,58 @@ class MainViewController: UIViewController {
             self.navigationItem.rightBarButtonItem = loginBtn
         }
     }
+    
+    @objc func indexChanged(_ sender: UISegmentedControl) {
+        
+        // 변경될 VC 지정
+        switch sender.selectedSegmentIndex {
+        case 0:
+            targetVC = homeViewController
+        default:
+            targetVC = realTimeViewController
+        }
+        
+    }
 }
 
 extension MainViewController {
     func setStyle() {
         view.backgroundColor = .black
+    }
+    
+    func setLayOut() {
+        
+        [targetVC.view, segmentedControl].forEach {
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            targetVC.view.topAnchor.constraint(equalTo: view.topAnchor),
+            targetVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            targetVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            targetVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    
+        segmentedControl.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(120)
+            $0.height.equalTo(50)
+            $0.leading.equalToSuperview().inset(10)
+            $0.trailing.equalToSuperview().inset(10)
+        }
+    }
+    
+    func setSegment() {
+        self.segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white,
+              .font: UIFont.systemFont(ofSize: 11, weight: .medium)], for: .normal)
+        self.segmentedControl.setTitleTextAttributes(
+            [
+                NSAttributedString.Key.foregroundColor: UIColor.white,
+                .font: UIFont.systemFont(ofSize: 11, weight: .semibold)
+            ],
+            for: .selected
+        )
+        self.segmentedControl.addTarget(self, action: #selector(indexChanged(_:)), for: .valueChanged)
+        self.segmentedControl.selectedSegmentIndex = 0
     }
     
     @objc
