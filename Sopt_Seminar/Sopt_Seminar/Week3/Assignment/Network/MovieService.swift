40bb477e554acf041ae120f2226ee061990f0ac3
김응관 <kwhn9486@naver.com>
2023-05-12 02:30:10 +0900

    [Add] API Request 코드추가 #13

--- Sopt_Seminar/Sopt_Seminar/Week3/Assignment/Network/MovieService.swift
+++ Sopt_Seminar/Sopt_Seminar/Week3/Assignment/Network/MovieService.swift
@@ -17,16 +17,49 @@ final class MovieService {
     private init() {}
     
     //75df104409445c36a314b12eb0aa5fdb
+    //ko
     
-    func getInfo(key: String, completion: @escaping (NetworkState<Any>) -> Void) {
+    func getInfo(key: String, language: String, page: Int, completion: @escaping (NetworkState<Any>) -> Void) {
         
-        let header: HTTPHeader = [ 
-        
+        let header: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 75df104409445c36a314b12eb0aa5fdb"
         ]
         
+        let url = MovieConfig.movieURL + "?language=\(language)&page=\(page)"
         
+        let dataRequest = AF.request(url, method: .get, headers: header)
+        
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let value = response.value else { return }
+                
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+            case .failure:
+                completion(.networkErr)
+            }
+        }
     }
     
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkState<Any> {
+        switch statusCode {
+        case 200: return isValidData(data: data)
+        case 201: return isValidData(data: data)
+        case 400, 409: return isValidData(data: data)
+        case 500: return .serverErr
+        default: return .networkErr
+        }
+    }
     
-    
+    private func isValidData(data: Data) -> NetworkState<Any> {
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode(Weathers.self, from: data)
+        else { return .pathErr }
+        
+        return .success(decodedData as Any)
+    }
 }
